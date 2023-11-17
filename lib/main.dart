abe0df89ea0c1f7010f8c7c86ea1629072068402
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2023-11-17 17:36:35 +0100

    Update Lista + Mensajes clickeables en lista

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,106 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: CrazyDisplay(),
     );
   }
 }
 
 class CrazyDisplay extends StatefulWidget {
+  const CrazyDisplay({super.key});
   @override
   _CrazyDisplayState createState() => _CrazyDisplayState();
 }
 
 class _CrazyDisplayState extends State<CrazyDisplay> {
+  String _serverIp = 'localhost';
+  final String _serverPort = '8888';
+
+  IOWebSocketChannel? _channel;
+
   TextEditingController ipController = TextEditingController();
   TextEditingController mensajeController = TextEditingController();
-  List<String> messageList = [];
+
+  List<String> mensajeList = [];
   bool isConnected = false;
   bool isSidebarOpen = false;
+
+  void _connectToServer() {
+    String server = "ws://$_serverIp:$_serverPort";
+    _channel = IOWebSocketChannel.connect(server);
+
+    _channel!.stream.listen(
+      (mensaje) {
+        final data = jsonDecode(mensaje);
+      },
+    );
+    Future delayed = Future.delayed(Duration(seconds: 3));
+    delayed.then((value) {
+      print("3 segundos despues");
+      checkConnection();
+    });
+  }
+
+  void checkConnection() {
+    if (_channel != null) {
+      _onOpen();
+      setState(() {
+        isConnected = true;
+      });
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text('Error al conectar')));
+      setState(() {
+        isConnected = false;
+      });
+    }
+  }
+
+  void _onOpen() {
+    _sendmensaje("~FlutterClient");
+    print("Conectado");
+  }
+
+  void _sendmensaje(String mensaje) {
+    if (_channel != null) {
+      _channel!.sink.add(mensaje);
+    }
+  }
+
+  _disconnectFromServer() {
+    _channel!.sink.close();
+  }
+
+  void addmensaje(String mensaje) {
+    bool repetido = false;
+    setState(() {
+      for (int i = 0; i < mensajeList.length; i++) {
+        if (mensajeList[i] == mensaje) {
+          repetido = true;
+        }
+      }
+      if (!repetido) {
+        mensajeList.add(mensaje);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('CrazyDisplay'),
+        title: const Text('CrazyDisplay'),
         centerTitle: true,
       ),
       body: Row(
@@ -61,33 +132,52 @@ class _CrazyDisplayState extends State<CrazyDisplay> {
                     children: [
                       ElevatedButton(
                         onPressed: () {
-                          setState(() {
-                            isConnected = !isConnected;
-                          });
+                          final ip = ipController.text;
+                          if (!isConnected && ip.isNotEmpty) {
+                            _serverIp = ip;
+                            _connectToServer();
+                          } else {
+                            _disconnectFromServer();
+                          }
                         },
                         child: Text(isConnected ? 'Desconectar' : 'Conectar'),
                       ),
                       ElevatedButton(
                         onPressed: () {
-                          // Implement logic to send the message
-                          String message = mensajeController.text;
-                          if (message.isNotEmpty) {
+                          // Implement logic to send the mensaje
+                          String mensaje = mensajeController.text;
+                          if (mensaje.isNotEmpty && isConnected) {
                             setState(() {
-                              messageList.add(message);
+                              _sendmensaje(mensaje);
+                              addmensaje(mensaje);
                               mensajeController.clear();
                             });
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content:
+                                        Text('Error al enviar el mensaje')));
                           }
                         },
-                        child: Text('Enviar'),
+                        child: const Text('Enviar'),
                       ),
                       ElevatedButton(
                         onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Hola David')));
-                          // Toggle the visibility of the sidebar
-                          setState(() {
-                            isSidebarOpen = !isSidebarOpen;
-                          });
+                          if (isConnected) {
+                            // Toggle the visibility of the sidebar
+                            setState(() {
+                              isSidebarOpen = !isSidebarOpen;
+                            });
+                          } else if (isSidebarOpen) {
+                            setState(() {
+                              isSidebarOpen = !isSidebarOpen;
+                            });
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        'Lista no disponible sin conexión')));
+                          }
                         },
                         child: Text('Mostrar Lista'),
                       ),
@@ -98,11 +188,11 @@ class _CrazyDisplayState extends State<CrazyDisplay> {
             ),
           ),
           if (isSidebarOpen)
-            Container(
+            SizedBox(
               width: 300, // Adjust the width as needed
               child: Drawer(
                 child: Container(
-                  padding: EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(50.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -114,10 +204,14 @@ class _CrazyDisplayState extends State<CrazyDisplay> {
                       Divider(),
                       Expanded(
                         child: ListView.builder(
-                          itemCount: messageList.length,
+                          itemCount: mensajeList.length,
                           itemBuilder: (context, index) {
                             return ListTile(
-                              title: Text(messageList[index]),
+                              title: Text(mensajeList[index]),
+                              onTap: () {
+                                // Add your logic to handle item click here
+                                print('Item clicked: ${mensajeList[index]}');
+                              },
                             );
                           },
                         ),
